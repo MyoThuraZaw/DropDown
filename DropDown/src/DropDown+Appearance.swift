7d055a49a3795bfedbe80b2d7c44bf44665ace22
Myo Thura Zaw <mtrzmyothurazaw@gmail.com>
2021-10-19 14:10:40 +0630

    Refactor shadow layer properties.

--- DropDown/src/DropDown+Appearance.swift
+++ DropDown/src/DropDown+Appearance.swift
@@ -20,10 +20,10 @@ extension DropDown {
 		appearance.selectionBackgroundColor = DPDConstant.UI.SelectionBackgroundColor
 		appearance.separatorColor = DPDConstant.UI.SeparatorColor
 		appearance.cornerRadius = DPDConstant.UI.CornerRadius
-		appearance.shadowColor = DPDConstant.UI.Shadow.Color
-		appearance.shadowOffset = DPDConstant.UI.Shadow.Offset
-		appearance.shadowOpacity = DPDConstant.UI.Shadow.Opacity
-		appearance.shadowRadius = DPDConstant.UI.Shadow.Radius
+		appearance.dropDownShadowColor = DPDConstant.UI.Shadow.Color
+		appearance.dropDownShadowOffset = DPDConstant.UI.Shadow.Offset
+		appearance.dropDownShadowOpacity = DPDConstant.UI.Shadow.Opacity
+		appearance.dropDownShadowRadius = DPDConstant.UI.Shadow.Radius
 		appearance.animationduration = DPDConstant.Animation.Duration
 		appearance.textColor = DPDConstant.UI.TextColor
         appearance.selectedTextColor = DPDConstant.UI.SelectedTextColor
